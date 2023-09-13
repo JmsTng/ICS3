b91ea37b10496da9381fbc4e981c6a87859c8449
JmsTng <james.tung+github@student.tdsb.on.ca>
2023-09-13 14:05:27 -0400

    minor changes

---
+++ src/HW3_Output1.java
@@ -0,0 +1,11 @@
+public class HW3_Output1 {
+    public static void main(String[] args) {
+        System.out.println(5);
+        System.out.println(" ".repeat(3) + 3);
+        System.out.println("5 * 2 = " + 5 * 2);
+        System.out.println("My name is" + "\t".repeat(2) + "George" + "!".repeat(4));
+        System.out.println(3.0 + " ".repeat(3) + 10.563 + " ".repeat(2) + "harry");
+        System.out.println("\n".repeat(3));
+        System.out.println("I think I got it!");
+    }
+}
