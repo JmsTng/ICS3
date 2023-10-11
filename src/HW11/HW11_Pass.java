1844e8055af5f44756292637141939e8cd55f9a7
JmsTng <james.ftung+github@gmail.com>
2023-10-11 08:20:11 -0400

    math one.

--- src/HW11/HW11_Pass.java
+++ src/HW11/HW11_Pass.java
@@ -1,33 +1,210 @@
-/*
- * TITLE: Homework Set 11 - Pass
- * NAME: James Tung
- * DATE: 10/7/2023
- * DESCRIPTION: Prompt the user with a math problem. Output whether the user got it right or wrong.
- */
+class HW11_Pass {
+    public static void main(String[] args) {
+        Symbol head = buildEquation(5);
 
-public class HW11_Pass {
-    public static void main (String[] args) {
-        // Generate random numbers
-        int terms = (int) (Math.random() * 9) + 2; // Number of terms
-        System.out.println(buildEquation(0, terms, new Object[32], 0));
+        System.out.println(outputEquation(head));
+        eval(head);
     }
 
-    public static Object buildEquation (int iteration, int iterationLimit, Object[] expr, double value) {
-        double valueCopy = value;
-        if (iteration == 0) {
-            int random = (int) (Math.random() * 1000);
-            expr[expr.length - 1] = random;
-            valueCopy = random;
-        } else {
-            // Generate random BEDMAS operator
-            int operator = (int) (Math.random() * 6);
+    public static Operator locate(Symbol head) {
+        Symbol last = head;
+        Operator max = null;
+        int maxPrecedence = -1;
+        while (last.next != null) {
+            if (last instanceof Operator op) {
+                if (op.precedence > maxPrecedence) {
+                    max = op;
+                    maxPrecedence = op.precedence;
+                }
+            }
+            last = last.next;
+        }
+        return max;
+    }
 
+    public static double eval(Symbol head) {
+        // Clear brackets
+        for (Symbol symbol = head; symbol != null; symbol = symbol.next) {
+            if (symbol instanceof Operator op) {
+                if (op.prev instanceof Operator || op.operator.equals(")")) {
+                    op.remove();
+                } else if (op.operator.equals("(")) {
+                    op.replace(new Operator(op.prev, " * ", op.precedence - 3));
+                }
+            }
         }
 
-        if (iteration == iterationLimit) {
-            return expr;
-        } else {
-            return buildEquation(iteration + 1, iterationLimit, expr, valueCopy);
+        System.out.println(outputEquation(head));
+
+        // Solve equation
+        while (locate(head) != null) {
+            Operator op = (Operator) locate(head);
+
+            op.operate();
+            System.out.println(outputEquation(head));
         }
+        assert head != null;
+        return ((Number) head).value;
+    }
+
+    public static String outputEquation(Symbol head) {
+        StringBuilder equation = new StringBuilder();
+        for (Symbol symbol = head; symbol != null; symbol = symbol.next) {
+            if (symbol instanceof Number num) {
+                if (num.value < 0) {
+                    equation.append("(").append(num).append(")");
+                } else {
+                    equation.append(num);
+                }
+            } else {
+                equation.append(symbol);
+            }
+        }
+        return equation.toString();
+    }
+
+    public static Number buildEquation(int terms) {
+        // Random first number
+        Number head = new Number(null, (int) (Math.random() * 500) - 250);
+
+        Symbol last = head;
+        int level = 0;
+        for (int i = 0; i < terms; i++) {
+            // Random operator
+            int op = (int) (Math.random() * 6);
+            switch (op) {
+                case 0 -> last = last.append(new Operator(last, " + ", level*3));
+                case 1 -> last = last.append(new Operator(last, " - ", level*3));
+                case 2 -> last = last.append(new Operator(last, " * ", level*3+1));
+                case 3 -> last = last.append(new Operator(last, " / ", level*3+1));
+                case 4 -> last = last.append(new Operator(last, "^", level*3+2));
+                case 5 -> { level++; last = last.append(new Operator(last, "(", level*3+3)); }
+            }
+
+            // Produce number
+            if (op != 4) {
+                last = last.append(new Number(last, (int) (Math.random() * 500) - 250));
+            } else {
+                last = last.append(new Number(last, (int) (Math.random() * 3) + 2));
+            }
+
+            // Possibly close bracket
+            if (Math.random() < 0.2 && level > 0) {
+                last = last.append(new Operator(last, ")", level*3+3));
+                level--;
+            }
+        }
+
+        while (level > 0) {
+            last = last.append(new Operator(last, ")", level*3+3));
+            level--;
+        }
+
+        return head;
+    }
+}
+
+class Symbol {
+    Symbol prev;
+    Symbol next;
+
+    public Symbol(Symbol prev) {
+        this.prev = prev;
+    }
+
+    public Symbol append(Symbol next) {
+        this.next = next;
+        next.prev = this;
+
+        return next;
+    }
+
+    public Symbol prepend(Symbol prev) {
+        this.prev = prev;
+        prev.next = this;
+
+        return prev;
+    }
+
+    public Symbol remove() {
+        if (prev != null) {
+            prev.next = next;
+        }
+        if (next != null) {
+            next.prev = prev;
+        }
+        prev = null;
+        next = null;
+
+        return this;
+    }
+
+    public Symbol replace(Symbol replacement) {
+        if (prev != null) {
+            prev.next = replacement;
+        }
+        replacement.prev = prev;
+        replacement.next = this.next;
+        if (next != null) {
+            next.prev = replacement;
+        }
+
+        return next;
+    }
+}
+
+class Number extends Symbol {
+    double value;
+
+    public Number(Symbol prev, double value) {
+        super(prev);
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        if (value == (int) value) {
+            return Integer.toString((int) value);
+        } else {
+            return Double.toString(value);
+        }
+    }
+}
+
+class Operator extends Symbol {
+    String operator;
+    int precedence;
+
+    public Operator(Symbol prev, String operator, int precedence) {
+        super(prev);
+        this.operator = operator;
+        this.precedence = precedence;
+    }
+
+    @Override
+    public String toString() {
+        return operator;
+    }
+
+    public double operate() {
+        System.out.print(prev);
+        System.out.print(this);
+        System.out.println(next);
+        double a = ((Number) prev).value;
+        double b = ((Number) next).value;
+        next.remove();
+        prev.remove();
+
+        double res = switch (operator) {
+            case " + " -> a + b;
+            case " - " -> a - b;
+            case " * " -> a * b;
+            case " / " -> a / b;
+            case "^" -> Math.pow(a, b);
+            default -> 0;
+        };
+
+        this.replace(new Number(prev, res));
+        return res;
     }
 }
