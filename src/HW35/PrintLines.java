19ef967523c3be36e110614755dbcfc18df4890f
James Tung <james.tung@student.tdsb.on.ca>
2023-12-01 15:12:25 -0500

    fakushjbgkuaeiwjhj

---
+++ src/HW35/PrintLines.java
@@ -0,0 +1,15 @@
+/*
+ * TITLE: Homework Set 35 - PrintLines
+ * NAME: James Tung
+ * DATE: 2023-12-01
+ * DESCRIPTION:
+ */
+
+package HW35;
+
+public class PrintLines {
+    public static void main(String[] args) {
+        // Initialize variables
+
+    }
+}
